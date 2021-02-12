80515604b037d28b366156da4a308f2e3401efb5
newt0 <py_lambda@yahoo.co.jp>
2021-02-12 18:28:04 +0900

    add: SingleSheet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from "react";
+import SingleSheet from "./SingleSheet";
 import "./App.css";
+
 import GoogleSheetsProvider from "react-db-google-sheets";
 
-const App = () => {
-  return (
-    <GoogleSheetsProvider>
-      <div>test</div>
-    </GoogleSheetsProvider>
-  );
-};
+const App = () => (
+  <GoogleSheetsProvider>
+    <div className="container">
+      <h1>Metis Examples</h1>
+      <div className="section">
+        <h2>Single Sheet Example</h2>
+        <SingleSheet />
+      </div>
+    </div>
+  </GoogleSheetsProvider>
+);
 
 export default App;
